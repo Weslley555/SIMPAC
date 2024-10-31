affad711655705a3f3322fe476f150cbc0154c16
Weslley555 <weslleyh.dias@gmail.com>
2024-10-31 14:51:14 -0300

    Correção do Problema no Hash

--- database/migrations/2024_10_31_154233_create_admins_table.php
+++ database/migrations/2024_10_31_154233_create_admins_table.php
@@ -6,28 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateAdminsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->string('nome'); // Adicionando a coluna 'nome'
-            $table->string('matricula')->unique();
+            $table->string('nome');
             $table->string('email')->unique();
             $table->string('senha');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('admins');
